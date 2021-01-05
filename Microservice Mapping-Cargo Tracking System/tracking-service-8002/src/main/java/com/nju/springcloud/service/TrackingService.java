b633c1bd188ad63e2d81b6813679b7990a859172
Weizerojust <963369304@qq.com>
2021-01-05 22:25:15 +0800

    add cargo tracking system (spring cloud)

---
+++ Microservice Mapping-Cargo Tracking System/tracking-service-8002/src/main/java/com/nju/springcloud/service/TrackingService.java
@@ -0,0 +1,8 @@
+package com.nju.springcloud.service;
+
+import com.nju.springcloud.entities.cargoVersion2.CargoVersion2;
+import org.apache.ibatis.annotations.Param;
+
+public interface TrackingService {
+    public CargoVersion2 ViewTracking(@Param("trackingId") String trackingId);
+}
